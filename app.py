e7b90a323763bb99ee2ad9a8a6a4b495a8e9b562
Sidharth Chhawchharia <sidharthchh@gmail.com>
2021-06-01 09:52:12 +0530

    Local testing environment

--- app.py
+++ app.py
@@ -2,39 +2,48 @@ from liveLambda_v2 import LiveLambdaV2
 
 app = LiveLambdaV2()
 
-localTesting = True
+localTesting = False
 lambdaTesting = False
+
 
 @app.route("/", "GET")
 def f1():
-  return {"key" : str(app.request), "route" : "GET /"}
+    return {"key": str(app.request), "route": "GET /"}
+
 
 @app.route("/", "POST")
 def f2():
-  return {"key" : str(app.request), "route" : "POST /"}
+    return {"key": str(app.request), "route": "POST /"}
+
 
 @app.route("/user", "POST")
 def f3():
-  return {"key" : str(app.request), "route" : "POST /user"}
+    return {"key": str(app.request), "route": "POST /user"}
+
 
 @app.route("/user", "GET")
 def f4():
-  return {"key" : str(app.request), "route" : "GET /user"}
+    return {"key": str(app.request), "route": "GET /user"}
+
 
 @app.route("/user/{id}", "POST")
 def f4(id):
-  return {"key" : str(app.request), "route" : "POST /user/" + str(id)}
+    return {"key": str(app.request), "route": "POST /user/" + str(id)}
+
 
 @app.route("/user/{id}", "GET")
 def f5(id):
-  return {"key" : str(app.request), "route" : "GET /user/" + str(id)}
+    return {"key": str(app.request), "route": "GET /user/" + str(id)}
 
-from dummy_event import Dummy_event, dummy_event_template
+
+from dummy_event import dummy_event_template
+
 
 def lambda_handler(event, context):
-  response = app.run(event, context)
-  print(response)
-  return response
+    response = app.run(event, context)
+    print(response)
+    return response
+
 
 if localTesting:
-  lambda_handler(dummy_event_template, {})
+    lambda_handler(dummy_event_template, {})
--- liveLambda_v2.py
+++ liveLambda_v2.py
@@ -1,136 +1,141 @@
 import json
 
-class Request:
-  # Base class for a request object. Every request object comming from API gateway is transformed internally into 
-  # LiveLambdaV2's internal request object. 
-  # REQUEST (API GATEWAY) --> REQUEST (LiveLambda)
-  
-  def __init__(self):
-      self.request_context = {}
-      self.headers = {}
-      self.body = {}
-      self.pathParameters = None
-      self.queryStringParameters = None
-      self.mics = {}
 
-  def __repr__(self):
-      line = "Request Context: "+ str(self.request_context) + "\n" + "Request Headers: " + str(self.headers) + "\n"
-      line = line + "Body: " + str(self.body) + "\n" + "Path Parameters: " + str(self.pathParameters) + "\n"
-      line = line + "Query Params: " + str(self.queryStringParameters) + "\n" + "Mics: " + str(self.mics) + "\n"
-      return line
+class Request:
+    # Base class for a request object. Every request object comming from API gateway is transformed internally into
+    # LiveLambdaV2's internal request object.
+    # REQUEST (API GATEWAY) --> REQUEST (LiveLambda)
+
+    def __init__(self):
+        self.request_context = {}
+        self.headers = {}
+        self.body = {}
+        self.pathParameters = None
+        self.queryStringParameters = None
+        self.mics = {}
+
+    def __repr__(self):
+        line = "Request Context: " + str(self.request_context) + "\n" + "Request Headers: " + str(self.headers) + "\n"
+        line = line + "Body: " + str(self.body) + "\n" + "Path Parameters: " + str(self.pathParameters) + "\n"
+        line = line + "Query Params: " + str(self.queryStringParameters) + "\n" + "Mics: " + str(self.mics) + "\n"
+        return line
+
 
 class Response:
-  def __init__(self):
-      self.body = {}
-      self.headers = {}
-  def get_dict(self):
-    return {"body" : self.body, "header": self.headers}
+    def __init__(self):
+        self.body = {}
+        self.headers = {}
+
+    def get_dict(self):
+        return {"body": self.body, "header": self.headers}
+
 
 class LiveLambdaV2:
-  # Main class which handles the flow from registering handler functions to calling them on correct routes.
-  # Evey LiveLambdaV2 webapp must contain atleast one object of this class.
+    # Main class which handles the flow from registering handler functions to calling them on correct routes.
+    # Evey LiveLambdaV2 webapp must contain atleast one object of this class.
 
-  def __init__(self):
-    self.__route_dict = { "POST" : {}, "GET" : {}, "PUT" : {},"DELETE" : {} ,"ANY" : {}, "PATCH" : {} }
-    self.__app_config = {}
-    self.__event = None
-    self.request = {}
-    self.response = {}
-  
-  def register_route(self, route, methods, handler):
-    # Internal method, should not be used directly. Instead use @app.route() decorator to register handler functions.
-    # Here 'app' refers to a LiveLambdaV2 Object.
-    # @route:string -> Endpoint  for which to register handler. 
-    # @methods:list -> List of methods for which hanlder should be registered to a given route.
-    # @handler:function -> Handler function to register.
-    
-    for method in methods:
-      self.__route_dict[method][route] = handler
+    def __init__(self):
+        self.__route_dict = {"POST": {}, "GET": {}, "PUT": {}, "DELETE": {}, "ANY": {}, "PATCH": {}}
+        self.__app_config = {}
+        self.__event = None
+        self.request = {}
+        self.response = {}
 
+    def register_route(self, route, methods, handler):
+        # Internal method, should not be used directly.
+        # Instead use @app.route() decorator to register handler functions.
+        # Here 'app' refers to a LiveLambdaV2 Object.
+        # @route:string -> Endpoint  for which to register handler.
+        # @methods:list -> List of methods for which hanlder should be registered to a given route.
+        # @handler:function -> Handler function to register.
 
-  def route(self, route, method):
-    # This is a decorator, usage is similar like route decorator in flask.
-    # usage : @app.route(route='/{id}', method=['GET'])
-    #         def home(id):
-    #           pass
-    # Here 'app' is a LiveLambda object.
-    # @route:string -> Endpoint  for which to register handler. 
-    # @method:list/string ->  List of methods for which hanlder should be registered to a given route.
+        for method in methods:
+            self.__route_dict[method][route] = handler
 
-    isMethodList = type(method) == type([])
-    if not isMethodList:
-      # If there is a single method, then converting it into list of method.
-      method = [method]
+    def route(self, route, method):
+        # This is a decorator, usage is similar like route decorator in flask.
+        # usage : @app.route(route='/{id}', method=['GET'])
+        #         def home(id):
+        #           pass
+        # Here 'app' is a LiveLambda object.
+        # @route:string -> Endpoint  for which to register handler.
+        # @method:list/string ->  List of methods for which hanlder should be registered to a given route.
 
-    def wrapper(handler):
-      self.register_route(route=route, methods=method, handler=handler)
-      return handler
-    return wrapper
+        isMethodList = type(method) == type([])
+        if not isMethodList:
+            # If there is a single method, then converting it into list of method.
+            method = [method]
 
-  def generate_route_keys(self):
-    # Returns a list of all routes registered with an particular LiveLambdaV2 object.
+        def wrapper(handler):
+            self.register_route(route=route, methods=method, handler=handler)
+            return handler
 
-    full_paths = []
-    for method in self.__route_dict.keys():
-      for route in self.__route_dict[method]:
-        p = method + " " + route
-        full_paths.append(p)
-    return full_paths
+        return wrapper
 
+    def generate_route_keys(self):
+        # Returns a list of all routes registered with an particular LiveLambdaV2 object.
 
-  def get_param_name(self, route):
-    # Internal method, should not be used directly.
-    # run method internally uses it to find out the name of url parameter in a registered route.
+        full_paths = []
+        for method in self.__route_dict.keys():
+            for route in self.__route_dict[method]:
+                p = method + " " + route
+                full_paths.append(p)
+        return full_paths
 
-    start = None
-    end = None
-    for i in range(len(route)):
-      if route[i] == "{":
-        start = i
-      if route[i] == "}":
-        end = i
-        break
-    return route[start+1 : end]
+    def get_param_name(self, route):
+        # Internal method, should not be used directly.
+        # run method internally uses it to find out the name of url parameter in a registered route.
 
-  def run(self, event, context):
-    # This method takes an event and context object provided by the AWS Lambda handler function and calls 
-    # appropriate handler internally and returns the proper response.
-    # event:dict -> Event dictionary which lambda_handler function (first function which AWS Lambda runtime calls) 
-    #     received as an argument.
-    # context:object -> Context Object which lambda_handler function (first function which AWS Lambda runtime calls) 
-    #     received as an argument.
+        start = None
+        end = None
+        for i in range(len(route)):
+            if route[i] == "{":
+                start = i
+            if route[i] == "}":
+                end = i
+                break
+        return route[start + 1: end]
 
-    self.request = Request()
-    self.response = Response()
+    def run(self, event, context):
+        # This method takes an event and context object provided by the AWS Lambda handler function and calls
+        # appropriate handler internally and returns the proper response.
+        # event:dict -> Event dictionary which lambda_handler function (first function which AWS Lambda runtime calls)
+        #     received as an argument.
+        # context:object -> Context Object which lambda_handler function (first function which AWS Lambda runtime calls)
+        #     received as an argument.
 
-    # Setup request object 
-    self.request.request_context = event["requestContext"]
-    self.request.headers = event["headers"]
+        self.request = Request()
+        self.response = Response()
 
-    if "body" in event.keys():
-      self.request.body = json.loads(event["body"])
-    else:
-      self.request.body =  {}
+        # Setup request object
+        self.request.request_context = event["requestContext"]
+        self.request.headers = event["headers"]
 
-    if "pathParameters" in event.keys():
-      self.request.pathParameters = event["pathParameters"]
-    else:
-      self.request.pathParameters = {}
+        if "body" in event.keys():
+            self.request.body = json.loads(event["body"])
+        else:
+            self.request.body = {}
 
-    if "queryStringParameters" in event.keys():
-      self.request.queryStringParameters = event["queryStringParameters"]
-    else:
-      self.request.queryStringParameters = {}
+        if "pathParameters" in event.keys():
+            self.request.pathParameters = event["pathParameters"]
+        else:
+            self.request.pathParameters = {}
 
-    self.request.mics = {"version" : event["version"], "routeKey" : event["routeKey"], "rawPath" : event["rawPath"], "rawQueryString" : event["rawQueryString"], "isBase64Encoded" : event["isBase64Encoded"]} 
+        if "queryStringParameters" in event.keys():
+            self.request.queryStringParameters = event["queryStringParameters"]
+        else:
+            self.request.queryStringParameters = {}
 
-    # Call appropriate handler
-    method, route = event["routeKey"].split()
-    handler = self.__route_dict[method][route]
-    if "pathParameters" in event.keys():
-      param_name = self.get_param_name(route)
-      response = handler(event["pathParameters"][param_name])
-    else:
-      response = handler()
+        self.request.mics = {"version": event["version"], "routeKey": event["routeKey"], "rawPath": event["rawPath"],
+                             "rawQueryString": event["rawQueryString"], "isBase64Encoded": event["isBase64Encoded"]}
 
-    return response
+        # Call appropriate handler
+        method, route = event["routeKey"].split()
+        handler = self.__route_dict[method][route]
+        if "pathParameters" in event.keys():
+            param_name = self.get_param_name(route)
+            response = handler(event["pathParameters"][param_name])
+        else:
+            response = handler()
+
+        return response
